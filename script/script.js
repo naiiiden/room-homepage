8619841f3a98cb11d6c2a1f739567ac6cec0c0dc
naiden <na4ko359@gmail.com>
2022-01-19 14:02:26 +0200

    mobile slider completed

--- script/script.js
+++ script/script.js
@@ -8,3 +8,32 @@ document.getElementById("hamburger").addEventListener("click", () => {
         document.getElementById("menu-icon").setAttribute("src", "images/icon-hamburger.svg");
     }
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+}
