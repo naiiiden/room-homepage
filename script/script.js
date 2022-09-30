eb8784655c1f98dae46d99204e1160f70b924229
naiiiden <na4ko359@gmail.com>
2022-09-30 12:29:22 +0300

    mobile menu button add aria-* attributes

--- script/script.js
+++ script/script.js
@@ -4,8 +4,12 @@ document.getElementById("hamburger").addEventListener("click", () => {
 
     if (document.getElementById("menu-icon").getAttribute("src") === "images/icon-hamburger.svg") {
         document.getElementById("menu-icon").setAttribute("src", "images/icon-close.svg");
+        document.querySelector("#hamburger").ariaLabel = "Close menu";
+        document.querySelector("#hamburger").ariaExpanded = "true";
     } else {
         document.getElementById("menu-icon").setAttribute("src", "images/icon-hamburger.svg");
+        document.querySelector("#hamburger").ariaLabel = "Open menu";
+        document.querySelector("#hamburger").ariaExpanded = "false";
     }
 });
 
